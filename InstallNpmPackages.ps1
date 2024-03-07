61287a978b03dbc841520dc35d10084de72f20b9
Bruno DaSilva <hi.dasilvab@gmail.com>
2024-03-07 10:04:39 -0600

    Adding npm script

---
+++ InstallNpmPackages.ps1
@@ -0,0 +1,24 @@
+<# 
+=> Description: 
+=> This script install node packages
+=> from every directory inside of a main direrctory 
+=> This works well in a microservice env where you are constantly updating packages. 
+#>
+
+$folders = Get-ChildItem ".\" -Directory
+$initPath = '.\'
+foreach ($entry in $folders) { 
+  $name = $entry.FullName
+  Write-Host $name
+  if($name -Match "-int-"){
+      Write-Host "Skipping int repo"
+      continue
+  }
+  Set-Location $name
+  if(Test-Path -Path package.json){
+      Write-Host "package.json found running npm install..."
+      vsts-npm-auth -config .npmrc
+      npm install
+  }
+  Set-Location $initpath
+}
